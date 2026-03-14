25f9bf6a64c0eeaed81cdf7072caa2cfce84af68
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 02:16:25 -0300

    feat: ip label on window

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+from utils import get_ip
 
 window = tk.Tk()
 window.title("TransferiX")
 window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 window.geometry("500x300")
 window.resizable(False, False)
+
+my_ip = tk.Label(window, text=f"{get_ip()}")
+my_ip.pack()
 
 window.mainloop()
