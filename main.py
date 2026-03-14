3fc87a8b11de565e591cacd03c1a69b6dc3b636f
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 03:31:41 -0300

    docs: update and add new comments

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ window_width = 500
 window_height = 300
 
 def center_window(window_width, window_height):
+    # get screen size
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
 
+    # calculate positions to center
     pos_x = (screen_width // 2) - (window_width // 2)
     pos_y = (screen_height // 2) - (window_height // 2)
 
+    # set window size and position
     window.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
 
 def on_send_click():
+    # style
     send_button.config(relief="sunken")
     receive_button.config(relief="raised")
 
+    # functionality
     my_ip.pack_forget()
     ip_frame.pack_forget()
     status_label.config(text="Seleccione los archivos a enviar")
 
 def on_receive_click():
+    # style
     send_button.config(relief="raised")
     receive_button.config(relief="sunken")
 
+    # functionality
     ip_frame.pack(side="top", fill="x", padx=5, pady=(0, 5))
     my_ip.pack()
     status_label.config(text="Esperando archivos...")
@@ -32,26 +39,27 @@ def on_receive_click():
     content_frame.pack_forget()
     content_frame.pack(fill="both", expand=True, padx=5)
 
+# window configuration
 window = tk.Tk()
 window.title("TransferiX")
 window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 window.resizable(False, False)
 
-# ip
+# ip frame
 ip_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
 ip_frame.pack(side="top", fill="x", padx=5, pady=(0, 5))
 
 my_ip = tk.Label(ip_frame, text=f"{get_ip()}", bg="white")
 my_ip.pack(pady=2)
 
-# content
+# content frame
 content_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
 content_frame.pack(fill="both", expand=True, padx=5)
 
 status_label = tk.Label(content_frame, text="Esperando archivos...", bg="white", wraplength=490)
 status_label.pack()
 
-# buttons
+# buttons frame
 buttons_frame = tk.Frame(window)
 buttons_frame.pack(side="bottom", fill="x")
 
