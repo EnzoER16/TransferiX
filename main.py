76d42d521fbf5983a5aa60ab8143d472e191443a
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 03:04:22 -0300

    feat: center window on screen function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 from utils import get_ip
 
+window_width = 500
+window_height = 300
+
+def center_window(window_width, window_height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    pos_x = (screen_width // 2) - (window_width // 2)
+    pos_y = (screen_height // 2) - (window_height // 2)
+
+    window.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
+
 def on_send_click():
     send_button.config(relief="sunken")
     receive_button.config(relief="raised")
@@ -12,7 +24,6 @@ def on_receive_click():
 window = tk.Tk()
 window.title("TransferiX")
 window.iconphoto(True, tk.PhotoImage(file="icon.png"))
-window.geometry("500x300")
 window.resizable(False, False)
 
 ip_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
@@ -35,5 +46,7 @@ receive_button.pack(side="left", expand=True, fill="x", padx=5, pady=5)
 
 send_button = tk.Button(buttons_frame, text="Enviar archivos", command=lambda: on_send_click())
 send_button.pack(side="left", expand=True, fill="x", padx=5, pady=5)
+
+center_window(window_width, window_height)
 
 window.mainloop()
