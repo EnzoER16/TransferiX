04521f0b60e332a1ed7bd76e21361f0dc57285f1
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 03:20:38 -0300

    docs: add comments

--- main.py
+++ main.py
@@ -37,18 +37,21 @@ window.title("TransferiX")
 window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 window.resizable(False, False)
 
+# ip
 ip_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
 ip_frame.pack(side="top", fill="x", padx=5, pady=(0, 5))
 
 my_ip = tk.Label(ip_frame, text=f"{get_ip()}", bg="white")
 my_ip.pack(pady=2)
 
+# content
 content_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
 content_frame.pack(fill="both", expand=True, padx=5)
 
 status_label = tk.Label(content_frame, text="Esperando archivos...", bg="white", wraplength=490)
 status_label.pack()
 
+# buttons
 buttons_frame = tk.Frame(window)
 buttons_frame.pack(side="bottom", fill="x")
 
