680acb7c6d38177881529305e2eef0775ec1cf17
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 02:26:32 -0300

    feat: ip frame

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 window.geometry("500x300")
 window.resizable(False, False)
 
-my_ip = tk.Label(window, text=f"{get_ip()}")
-my_ip.pack()
+ip_frame = tk.Frame(window, bg="white", relief="groove", borderwidth=1)
+ip_frame.pack(side="top", fill="x", padx=5, pady=(0, 5))
+
+my_ip = tk.Label(ip_frame, text=f"{get_ip()}", bg="white")
+my_ip.pack(pady=2)
 
 window.mainloop()
