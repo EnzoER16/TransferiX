7315cc4c8264ae435489994c3c77c523fc86ca4f
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-03-14 00:42:43 -0300

    feat: window icon

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("TransferiX")
+window.iconphoto(True, tk.PhotoImage(file="icon.png"))
 window.geometry("500x300")
 window.resizable(False, False)
 
