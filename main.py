14a02cdf15904a5a67718ccb2a9b9f9bad868b6c
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-04-10 13:06:48 -0300

    feat: centered window function

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import customtkinter as ctk
 
+def center_window():
+    window.update_idletasks()
+
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x_position = (screen_width // 2) - (window_width // 2)
+    y_position = (screen_height // 2) - (window_height // 2)
+
+    window.geometry(f"+{x_position}+{y_position}")
+
 window = ctk.CTk()
 window.title("TransferiX")
 window.iconbitmap("assets/icon.ico")
 window.geometry("500x300")
 window.resizable(False, False)
+
+center_window()
 
 window.mainloop()
