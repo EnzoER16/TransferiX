e7101d7152503484e87a072a6919ec7c5565719e
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-08-22 22:24:18 -0300

    add file selection functionality

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import flet as ft, device_info
 def main(page: ft.Page):
     page.title = "TransferiX"
 
+    files_selected = []
+
+    async def select_files():
+        files = await ft.FilePicker().pick_files(dialog_title="Select files", allow_multiple=True)
+        if files:
+            for file in files:
+                files_selected.append(file.path)
+
     my_ip = device_info.get_local_ip()
 
 if __name__ == "__main__":
